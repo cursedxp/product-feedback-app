04d3d074a3cda028cfd2ada7bea9594bcd319d24
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 16:54:36 +0200

    html layout of mobile filter added

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,18 @@
+import ArrowIconDown from "../../assets/icons/icon-arrow-down.svg";
 export default function Filter() {
   return (
     <div>
-      <h3>Filter</h3>
+      <div>
+        <div>Sortby :</div>
+        <div>
+          <span>Most Upvotes</span>
+          <img src={ArrowIconDown} alt="" />
+        </div>
+      </div>
+      <button>
+        <img src="" alt="" />
+        Add Feedback
+      </button>
     </div>
   );
 }
