346dc2fe31d9593ee3ef3a37f643d9fdc95c5720
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 14:59:16 +0200

    structure update of the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home.page";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
