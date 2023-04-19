987527839cb7aa1e5e25b7a6d4f508e556c116a9
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 15:30:19 +0200

    pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Roadmap from "./pages/Roadmap/Roadmap";
+import Feedback from "./pages/Feedback/Feedback";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/roadmap" element={<Roadmap />} />
+        <Route path="/feedback" element={<Feedback />} />
       </Routes>
     </div>
   );
