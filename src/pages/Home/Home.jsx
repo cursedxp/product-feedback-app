d8d272dc1ec2864030b83bc8c8c177251bb2006d
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 23:58:33 +0200

    List Feedbacks on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
 import Feedback from "../../components/Feedback/Feedback";
 import Filter from "../../components/Filter/Filter";
+import { useEffect } from "react";
+import { getData } from "../../data/dataSlice";
 
 export default function Home() {
+  // Select the data slice of the Redux store
+  const feedbacks = useSelector((state) => {
+    return state.data;
+  });
+
+  // Get the productRequests array from the feedbacks data, or use an empty array if it doesn't exist
+  const requests = feedbacks?.data?.productRequests || [];
+  // Get the dispatch function from the useDispatch hook
+  const dispatch = useDispatch();
+
+  // Render a Feedback component for each request in the productRequests array
+  const feedback = requests.map((request) => {
+    return <Feedback request={request} key={request.id} />;
+  });
+
+  useEffect(() => {
+    // Dispatch the getData action creator function to get the data from the server and update the Redux store
+    dispatch(getData());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Home</h1>
       <Filter />
-      <Feedback />
+      {feedback}
     </div>
   );
 }
