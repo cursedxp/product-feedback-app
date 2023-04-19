24f7a184bec24f186b81099c2e203d63f03a8c2d
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 16:25:36 +0200

    menu will be hidden on specific pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Roadmap from "./pages/Roadmap/Roadmap";
 import Feedback from "./pages/Feedback/Feedback";
+import Header from "./components/Header/Header";
 
 function App() {
+  //This hook returns the current location object
+  const location = useLocation();
+  const pathLocation = location.pathname;
+
+  //List of paths to hide header
+  const hideHeaderOnPaths = ["/feedback"];
+  const hideHeader = hideHeaderOnPaths.includes(pathLocation);
+
   return (
     <div className="App">
+      {/* Hides header for specific routes */}
+      {!hideHeader && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/roadmap" element={<Roadmap />} />
