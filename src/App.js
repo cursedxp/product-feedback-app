b40babb380230f3d9d478f8dd6179afec1362766
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 16:29:21 +0200

    some comments added to app.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Feedback from "./pages/Feedback/Feedback";
 import Header from "./components/Header/Header";
 
 function App() {
-  //This hook returns the current location object
+  // Get the current URL location using the useLocation hook from react-router-dom
   const location = useLocation();
   const pathLocation = location.pathname;
 
-  //List of paths to hide header
+  // Define an array of paths where the header should be hidden
   const hideHeaderOnPaths = ["/feedback"];
+
+  // Check if the current path is in the hideHeaderPaths array using the includes method
   const hideHeader = hideHeaderOnPaths.includes(pathLocation);
 
   return (
     <div className="App">
-      {/* Hides header for specific routes */}
+      {/* Conditionally render the Header component based on the value of hideHeader  */}
       {!hideHeader && <Header />}
+
+      {/* Define the routes for the application using the Routes and Route components */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/roadmap" element={<Roadmap />} />
