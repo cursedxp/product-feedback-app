e0316fcfc7540ed7131b6765641f750fb42dedd7
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 23:58:52 +0200

    feedbacks props update

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,18 +1,21 @@
 import CommetIcon from "../../assets/icons/icon-comments.svg";
 import ArrowIconUp from "../../assets/icons/icon-arrow-up.svg";
-export default function Feedback() {
+export default function Feedback({ request }) {
+  const { title, description, category, upvotes, comments } = request;
+
   return (
     <div>
-      <div>Add tags for solutions</div>
-      <div>Easier to search for solutions based on a specific stack.</div>
-      <div>Enhancement</div>
+      <div>{title}</div>
+      <div>{description}</div>
+      <div>{category}</div>
       <div>
         <div>
           <img src={ArrowIconUp} alt="" />
-          112
+          {upvotes}
         </div>
         <div>
-          <img src={CommetIcon} alt="" />2
+          <img src={CommetIcon} alt="" />
+          {comments && comments.length}
         </div>
       </div>
     </div>
