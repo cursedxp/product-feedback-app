a48dd95dd813be42d5fed22d8fc55cc6c1f85656
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-19 23:06:10 +0200

    store provide has been added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "normalize.css";
 import "./styles/main.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
