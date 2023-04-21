3c2b2ed16f5e646f34ca23bee76d69b777b3f594
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-21 16:08:22 +0200

    filter component has been created

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -1,10 +1,16 @@
-export default function CategoryFilter() {
+import { v4 as uuidv4 } from "uuid";
+export default function CategoryFilter({ categories }) {
   return (
     <div className="category-filter">
-      <input type="radio" name="category" value="" />
-      <input type="radio" name="category" value="" />
-      <input type="radio" name="category" value="" />
-      <input type="radio" name="category" value="" />
+      {categories.map((category) => {
+        const uniqueId = uuidv4();
+        return (
+          <div key={uniqueId}>
+            <input type="radio" name="category" value={category} />
+            <label htmlFor={category}>{category}</label>
+          </div>
+        );
+      })}
     </div>
   );
 }
