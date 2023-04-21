3fc81c28f70b79fe35ddaaa79198d6bf13e72b90
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-21 15:16:25 +0200

    category component has  been created

---
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -0,0 +1,10 @@
+export default function CategoryFilter() {
+  return (
+    <div className="category-filter">
+      <input type="radio" name="category" value="" />
+      <input type="radio" name="category" value="" />
+      <input type="radio" name="category" value="" />
+      <input type="radio" name="category" value="" />
+    </div>
+  );
+}
