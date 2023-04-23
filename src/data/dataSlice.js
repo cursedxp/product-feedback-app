ab50491f9e5276b8c9cb5eb0e237d711019d1aa0
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-23 19:05:08 +0200

    initial state moved inside slice

--- src/data/dataSlice.js
+++ src/data/dataSlice.js
@@ -1,19 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const initialState = {
-  data: [],
-  loading: false,
-  error: "",
-};
-
 export const getData = createAsyncThunk("data/getData", () => {
   return axios.get("./data.json").then((response) => response.data);
 });
 
 const dataSlice = createSlice({
   name: "data",
-  initialState,
+  initialState: {
+    data: [],
+    loading: false,
+    error: "",
+  },
+
   extraReducers: (builder) => {
     builder.addCase(getData.pending, (state) => {
       state.loading = true;
