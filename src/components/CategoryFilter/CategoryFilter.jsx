a06e2b37ad308450462772ee2ad96100f8c34723
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-23 19:06:13 +0200

    radio button adde for all feebacks

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -2,11 +2,20 @@ import { v4 as uuidv4 } from "uuid";
 export default function CategoryFilter({ categories }) {
   return (
     <div className="category-filter">
+      <div>
+        <input type="radio" name="category" value={""} onChange={""} />
+        <label htmlFor="all">all</label>
+      </div>
       {categories.map((category) => {
         const uniqueId = uuidv4();
         return (
           <div key={uniqueId}>
-            <input type="radio" name="category" value={category} />
+            <input
+              type="radio"
+              name="category"
+              value={category}
+              onChange={""}
+            />
             <label htmlFor={category}>{category}</label>
           </div>
         );
