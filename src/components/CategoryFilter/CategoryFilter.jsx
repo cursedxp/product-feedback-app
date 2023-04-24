802ee780d0fd54220f7c6b1025238c4077d671e3
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-24 15:52:55 +0200

    filter handler has been added to category filter

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -1,9 +1,26 @@
 import { v4 as uuidv4 } from "uuid";
+import { setFilter } from "../../data/dataSlice";
+import { useDispatch } from "react-redux";
+
 export default function CategoryFilter({ categories }) {
+  const dispatch = useDispatch();
+
+  //Set filter category when user clicks on radio buttons
+  const handleFilter = (categoryName) => {
+    dispatch(setFilter(categoryName));
+  };
+
   return (
     <div className="category-filter">
       <div>
-        <input type="radio" name="category" value={""} onChange={""} />
+        <input
+          type="radio"
+          name="category"
+          value="all"
+          onChange={() => {
+            handleFilter("all");
+          }}
+        />
         <label htmlFor="all">all</label>
       </div>
       {categories.map((category) => {
@@ -14,7 +31,9 @@ export default function CategoryFilter({ categories }) {
               type="radio"
               name="category"
               value={category}
-              onChange={""}
+              onChange={() => {
+                handleFilter(category);
+              }}
             />
             <label htmlFor={category}>{category}</label>
           </div>
