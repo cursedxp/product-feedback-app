3098ff52caa42843f43aaeebc08c6c0882b1eb82
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-25 19:48:59 +0200

    simple html structure of comments

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -4,6 +4,7 @@ import { getData } from "../../data/dataSlice";
 import { useEffect, useMemo } from "react";
 import FeedbackComp from "../../components/Feedback/Feedback";
 import "./Feedback.scss";
+import { Link } from "react-router-dom";
 
 export default function Feedback() {
   const { id } = useParams();
@@ -24,6 +25,32 @@ export default function Feedback() {
   return (
     <div className="feedback-details">
       <FeedbackComp item={feedback} />
+      <div className="comments">
+        <div className="section-header">
+          <span>{feedback.comments.length}</span>
+          <span>Comments</span>
+        </div>
+        {feedback.comments.map((comment) => {
+          return (
+            <div className="comment">
+              <div className="author-details">
+                <img src={comment.user.image} alt={comment.user.name} />
+                <div className="flex column">
+                  <div className="author-name">{comment.user.name}</div>
+                  <div className="author-username">
+                    @{comment.user.username}
+                  </div>
+                </div>
+                <Link>Reply</Link>
+                {console.log(feedback.comments)}
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
