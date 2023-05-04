368699d8f41d1f5fd2e8d71ab4a8603327f6c6ca
Anil Ozsoy <anilozsoy@gmail.com>
2023-05-04 14:19:29 +0200

    sort-filter func added to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 import Feedback from "../../components/Feedback/Feedback";
 import Filter from "../../components/Filter/Filter";
 import { useEffect } from "react";
-import { getData, filteredFeedbacks } from "../../data/dataSlice";
+import { getData, sortAndFilterFeedbacks } from "../../data/dataSlice";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  const filteredItems = useSelector(filteredFeedbacks);
+  const filteredItems = useSelector(sortAndFilterFeedbacks);
 
   // Get the dispatch function from the useDispatch hook
   const dispatch = useDispatch();
