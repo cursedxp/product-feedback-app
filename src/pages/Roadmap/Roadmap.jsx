e1d68c83b9485ce8a7b4880f76b23461ab69c76e
Anil Ozsoy <anilozsoy@gmail.com>
2023-05-05 14:36:23 +0200

    page functionality has been completed

--- src/pages/Roadmap/Roadmap.jsx
+++ src/pages/Roadmap/Roadmap.jsx
@@ -1,7 +1,60 @@
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getData, setStatus, filteredByStatus } from "../../data/dataSlice";
+import { useEffect, useMemo } from "react";
+import { v4 as uuidv4 } from "uuid";
 export default function Roadmap() {
+  const dispatch = useDispatch();
+  const feedbacksByStatus = useSelector(filteredByStatus);
+  const memoizedFeedbacksByStatus = useMemo(
+    () => feedbacksByStatus,
+    [feedbacksByStatus]
+  );
+  const statuses = new Set(
+    useSelector((state) => {
+      return state.data.data.productRequests?.map(
+        (feedback) => feedback.status
+      );
+    })
+  );
+  const memoizedStatuses = useMemo(() => statuses, [statuses]);
+
+  useEffect(() => {
+    dispatch(setStatus("planned"));
+    dispatch(getData());
+  }, [dispatch]);
+
+  const handleSort = (status) => {
+    dispatch(setStatus(status));
+  };
+
+  console.log(memoizedFeedbacksByStatus);
+
   return (
     <div>
-      <h1>Roadmap</h1>
+      <div className="nav">
+        <Link>
+          <img src="" alt="" />
+          <span>Go Back</span>
+        </Link>
+        <button>+ Add Feedback</button>
+      </div>
+      <div className="roadmap-filter">
+        {Array.from(memoizedStatuses).map((status) => {
+          const uuid = uuidv4();
+          return (
+            <div
+              key={uuid}
+              onClick={() => {
+                handleSort(status);
+              }}
+            >
+              {status}
+            </div>
+          );
+        })}
+      </div>
+      <div className="feedback-list"></div>
     </div>
   );
 }
