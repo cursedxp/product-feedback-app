55bbdcd01190a729cc7a6c4588ebc8ea4e4188c3
Anil Ozsoy <anilozsoy@gmail.com>
2023-05-11 12:56:10 +0200

    roadmap widget has been created

---
+++ src/components/RoadmapWidget/RoadMapWidget.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+export default function RoadMapWidget() {
+  return (
+    <div className="road-map">
+      <div className="flex space-between align-center">
+        <span className="card-title">Roadmap</span>
+        <Link className="link">View</Link>
+      </div>
+      <ul>
+        <li>
+          <div>
+            <span className="orange"></span>
+            <span className="status">Planned</span>
+          </div>
+          <span className="total">2</span>
+        </li>
+        <li>
+          <div>
+            <span className="violet"></span>
+            <span className="status">In-Progress</span>
+          </div>
+          <span className="total">3</span>
+        </li>
+        <li>
+          <div>
+            <span className="blue"></span>
+            <span className="status">Live</span>
+          </div>
+          <span className="total">1</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
